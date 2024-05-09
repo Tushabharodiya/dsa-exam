9338a1214f26f1f5bb9849c0f89e654cdcf3d770
tusha <4341tushabharodiya@gmail.com>
2024-05-09 10:30:54 +0530

    exam

---
+++ kadence.js
@@ -0,0 +1,19 @@
+let arr=[-2,-3,4,-1,1,5,-3]
+
+let max=0;
+let current=arr[0]
+
+
+for(let i=0;i<arr.length;i++){
+    current=current+arr[i];
+
+    if(current<0){
+      current=0;
+    }
+
+    if(max<current){
+        max=current;
+    }
+}
+
+console.log("max sum in array",max);
---
+++ set.js
@@ -0,0 +1,30 @@
+//===>set impliment 
+
+// let list=new Set()
+// list.add(20)
+// list.add(21)
+// list.add(22)
+// list.add(23)
+
+// list.delete(22)
+// list.forEach((val)=>{    
+//     console.log(val);
+// })
+// list.clear()
+
+
+///map impliment
+
+
+let list =new Map()
+list.set("name","tusha")
+list.set("name1","krishna")
+list.set("name2","sarika")
+list.set("name3","ishika")
+
+list.get("name1")
+list.delete("name2")
+list.has("tusha")
+list.forEach((val)=>{
+    console.log(val);
+})
